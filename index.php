c7bcb55afcd15e473e95e91d2df2f8c11c123e11
Donovan Tengblad <purplefish32@gmail.com>
2013-05-27 16:37:21 +0200

    Fix

--- index.php
+++ index.php
@@ -4,25 +4,9 @@
     <meta charset="utf-8">
     <title>Creation d'une formation pour Claroline connect</title>
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css" type="text/css"/>
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
-
-    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-    <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
-
-    <!-- <script src="jquery.js" type="text/javascript"></script>  -->
-    <script src="bootstrap/js/bootstrap.min.js " type="text/javascript"></script>
-    <script src="script.js " type="text/javascript"></script>
-
-    <script type="text/javascript" src="jstree/_lib/jquery.cookie.js"></script>
-    <script type="text/javascript" src="jstree/_lib/jquery.hotkeys.js"></script>
-    <script type="text/javascript" src="jstree/jquery.jstree.js"></script>
-    <link type="text/css" rel="stylesheet" href="jstree/_docs/syntax/!style.css"/>
-    <link type="text/css" rel="stylesheet" href="jstree/_docs/!style.css"/>
-    <script type="text/javascript" src="jstree/_docs/syntax/!script.js"></script>
-    <script type="text/javascript" src="jstree/_demo/propre.js"></script>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
 </head>
-
 <body>
 <div class="container" id="contain">
     <?php include("php/navbar.php"); ?>
@@ -31,5 +15,13 @@
 </div>
     <?php include("php/volet-left.php"); ?>
     <?php include("php/volet-right.php"); ?>
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js " type="text/javascript"></script>
+    <script type="text/javascript" src="jstree/_lib/jquery.cookie.js"></script>
+    <script type="text/javascript" src="jstree/_lib/jquery.hotkeys.js"></script>
+    <script type="text/javascript" src="jstree/jquery.jstree.js"></script>
+    <script src="script.js " type="text/javascript"></script>
+    <script type="text/javascript" src="jstree/_demo/propre.js"></script>
 </body>
 </html>
