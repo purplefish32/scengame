6a3e33d380e41e7a9b72d4d1746500a9cb1c34f4
Donovan Tengblad <purplefish32@gmail.com>
2013-05-27 15:44:44 +0200

    Netoyage du code

--- index.php
+++ index.php
@@ -1,36 +1,35 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Creation d'une formation pour Claroline connect</title>
-<link rel="stylesheet" href="bootstrap/css/bootstrap.css" type="text/css"/>
-<link rel="stylesheet" href="style.css" type="text/css"/>
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
+    <meta charset="utf-8">
+    <title>Creation d'une formation pour Claroline connect</title>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css" type="text/css"/>
+    <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
 
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
+    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
 
-<!-- <script src="jquery.js" type="text/javascript"></script>  -->
-<script src="bootstrap/js/bootstrap.min.js " type="text/javascript"></script>
-<script src="script.js " type="text/javascript"></script>
+    <!-- <script src="jquery.js" type="text/javascript"></script>  -->
+    <script src="bootstrap/js/bootstrap.min.js " type="text/javascript"></script>
+    <script src="script.js " type="text/javascript"></script>
 
-<script type="text/javascript" src="jstree/_lib/jquery.cookie.js"></script>
-<script type="text/javascript" src="jstree/_lib/jquery.hotkeys.js"></script>
-<script type="text/javascript" src="jstree/jquery.jstree.js"></script>
-<link type="text/css" rel="stylesheet" href="jstree/_docs/syntax/!style.css"/>
-<link type="text/css" rel="stylesheet" href="jstree/_docs/!style.css"/>
-<script type="text/javascript" src="jstree/_docs/syntax/!script.js"></script>
-<script type="text/javascript" src="jstree/_demo/propre.js"></script>
-
+    <script type="text/javascript" src="jstree/_lib/jquery.cookie.js"></script>
+    <script type="text/javascript" src="jstree/_lib/jquery.hotkeys.js"></script>
+    <script type="text/javascript" src="jstree/jquery.jstree.js"></script>
+    <link type="text/css" rel="stylesheet" href="jstree/_docs/syntax/!style.css"/>
+    <link type="text/css" rel="stylesheet" href="jstree/_docs/!style.css"/>
+    <script type="text/javascript" src="jstree/_docs/syntax/!script.js"></script>
+    <script type="text/javascript" src="jstree/_demo/propre.js"></script>
 </head>
 
 <body>
 <div class="container" id="contain">
-<?php include("php/navbar.php"); ?>
-<?php include("php/central.php"); ?>
-<?php include("php/footer.php"); ?>
+    <?php include("php/navbar.php"); ?>
+    <?php include("php/central.php"); ?>
+    <?php include("php/footer.php"); ?>
 </div>
-<?php include("php/volet-left.php"); ?>
-<?php include("php/volet-right.php"); ?>
+    <?php include("php/volet-left.php"); ?>
+    <?php include("php/volet-right.php"); ?>
 </body>
 </html>
